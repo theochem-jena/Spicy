08de4a6984663dc32bd0409cbd01a8e4255313e1
Phillip Seeber <seeber@kreta.ipc3.uni-jena.de>
2018-07-25 17:18:29 +0200

    adding forgot coupled cluster and scf types

--- src/Spicy/Types.hs
+++ src/Spicy/Types.hs
@@ -23,25 +23,51 @@ module Spicy.Types
 , molecule_Gradient
 , molecule_Hessian
 --
-, Task
-, Software
-, Basis
-, Charge
-, Multiplicity
-, SE_Method
-, HF_Approx
-, DFT_Functional
-, DFT_Approx
-, MP_Order
-, MP_Flavour
-, MP_Approx
-, CC_Order
-, CC_Flavour
-, CC_Approx
-, CAS_Space
-, CAS_Roots
-, CAS_Weights
-, CAS_Approx
+, Task(..)
+, Software(..)
+, Basis(..)
+, Charge(..)
+, Multiplicity(..)
+, SE_Method(..)
+, HF_Approx(..)
+, DFT_Functional(..)
+, DFT_Approx(..)
+, MP_Order(..)
+, MP_Flavour(..)
+, MP_Approx(..)
+, CC_Order(..)
+, CC_Flavour(..)
+, CC_Approx(..)
+, CAS_Space(..)
+, cas_Space_nElec
+, cas_Space_Orbs
+, CAS_Roots(..)
+, CAS_Weights(..)
+, CAS_Approx(..)
+--
+, QC_SE_Method(..)
+, qc_se_Method
+, qc_se_Shell
+, QC_HF_Shell(..)
+, qc_hf_Approx
+, qc_hf_Shell
+, QC_DFT_Functional(..)
+, qc_dft_Functional
+, qc_dft_Approx
+, QC_MPN_Flavour(..)
+, qc_mpn_Flavour
+, qc_mpn_Approx
+, QC_MPN_Order(..)
+, qc_mpn_Order
+, qc_mpn_Flavour'
+, QC_CAS_Approx(..)
+, qc_cas_Approx
+, Methods(..)
+, methods_qc_SE
+, methods_qc_HF
+, methods_qc_DFT
+, methods_qc_MPN
+, methods_qc_CAS
 ) where
 import           Data.Map              (Map)
 import qualified Data.Map              as Map
@@ -164,6 +190,13 @@ data Basis =
 type Charge = Int
 type Multiplicity = Int
 
+-- | Type of the wavefunction
+data Shell =
+    Restricted
+  | Unrestricted
+  | RestrictedOpen
+  deriving (Show, Eq)
+
 -- | Semiempiricism
 -- |   -> Available semiempirical hamiltonians
 data SE_Method =
@@ -209,33 +242,66 @@ instance Show HF_Approx where
 -- |   -> Approximations
 data DFT_Functional =
   -- LDA
-    DFT_LDA
+    DFT_PWLDA
+  | DFT_VWN3
+  | DFT_VWN5
   -- GGA
-  | DFT_BP86
-  | DFT_PBE
   | DFT_BLYP
+  | DFT_OLYP
+  | DFT_GLYP
+  | DFT_XLYP
+  | DFT_PW91
+  | DFT_MPWPW
+  | DFT_MPWLYP
+  | DFT_PBE
+  | DFT_RPBE
+  | DFT_REVPBE
+  | DFT_PWP
   -- metaGGA
   | DFT_M06_L
   | DFT_M11_L
   | DFT_TPSS
   -- Hybrid
+  | DFT_B1LYP
   | DFT_B3LYP
+  | DFT_O3LYP
+  | DFT_X3LYP
+  | DFT_BP86
   | DFT_PBE0
+  | DFT_BHANDHLYP
   -- metaHybrid
-  | DFT_TPSSh
   | DFT_M06
   | DFT_M06_2X
-  | DFT_M11
+  | DFT_TPSS0
+  | DFT_TPSSh
+  -- range separated Hybrid
+  | WB97
+  | WB97X
+  | WB97X_D3
+  | CAM_B3LYP
+  | LV_BLYP
   -- doubleHybrid
-  | DFT_B2PYL
+  | DFT_B2PLYP
+  | DFT_B2PLYP_RI
+  | DFT_B2PLYP_D
+  | DFT_B2PLYP_D3
+  | DFT_MPW2PLYP
+  | DFT_MPW2PLYP_D
+  | DFT_B2GP_PLYP
+  | DFT_B2K_PLYP
+  | DFT_B2T_PLYP
+  | DFT_PWPB95
+  | DFT_PBPB95_RI
   deriving (Eq, Show)
 data DFT_Approx =
-    DFT_RIJ
+    DFT_None
+  | DFT_RIJ
   | DFT_RIJK
   | DFT_RIJCOSX
   | DFT_RIJONX
   deriving (Eq)
 instance Show DFT_Approx where
+  show DFT_None = "none"
   show DFT_RIJ = "RI-J"
   show DFT_RIJK = "RI-JK"
   show DFT_RIJCOSX = "RI-JCOSX"
@@ -338,7 +404,8 @@ data CAS_Space = CAS_Space
 makeLenses ''CAS_Space
 instance Show CAS_Space where
   show a =
-    ""
+    printf "%10s | %10s\n" "electrons" "orbitals" ++
+    printf "%10d | %10d %s"   (a ^. cas_Space_nElec) (length $ a ^. cas_Space_Orbs) (show $ a ^. cas_Space_Orbs)
 type CAS_Roots = Int
 type CAS_Weights = [Double]
 data CAS_Approx =
@@ -354,22 +421,26 @@ data CAS_Approx =
 -- | of possible QC combination
 data QC_SE_Method = QC_SE_Method
   { _qc_se_Method :: SE_Method
+  , _qc_se_Shell :: [Shell]
   } -- deriving (Eq, Show)
 makeLenses ''QC_SE_Method
 
-newtype QC_HF_Approx = QC_HF_Approx
-  { _qc_hf_Approx :: HF_Approx
-  } -- deriving (Eq, Show)
-makeLenses ''QC_HF_Approx
+data QC_HF_Shell = QC_HF_Shell
+  { _qc_hf_Shell :: Shell
+  , _qc_hf_Approx :: [HF_Approx]
+  }
+makeLenses ''QC_HF_Shell
 
 data QC_DFT_Functional = QC_DFT_Functional
   { _qc_dft_Functional :: DFT_Functional
+  , _qc_dft_Shell :: [Shell]
   , _qc_dft_Approx :: [DFT_Approx]
   }
 makeLenses ''QC_DFT_Functional
 
 data QC_MPN_Flavour = QC_MPN_Flavour
   { _qc_mpn_Flavour :: MP_Flavour
+  , _qc_mpn_Shell   :: [Shell]
   , _qc_mpn_Approx  :: [MP_Approx]
   } -- deriving (Eq, Show)
 makeLenses ''QC_MPN_Flavour
@@ -379,6 +450,18 @@ data QC_MPN_Order = QC_MPN_Order
   } -- deriving (Eq, Show)
 makeLenses ''QC_MPN_Order
 
+data QC_CC_Flavour = QC_CC_Flavour
+  { _qc_cc_Flavour :: CC_Flavour
+  , _qc_cc_Shell :: [Shell]
+  , _qc_cc_Approx :: [CC_Approx]
+  }
+makeLenses ''QC_CC_Flavour
+data QC_CC_Order = QC_CC_Order
+  { _qc_cc_Order :: CC_Order
+  , _qc_cc_Flavour' :: [QC_CC_Flavour]
+  }
+makeLenses ''QC_CC_Order
+
 newtype QC_CAS_Approx = QC_CAS_Approx
   { _qc_cas_Approx :: CAS_Approx
   } -- deriving (Eq, Show)
@@ -386,50 +469,105 @@ makeLenses ''QC_CAS_Approx
 
 data Methods = Methods
   { _methods_qc_SE  :: [QC_SE_Method]
-  , _methods_qc_HF  :: [QC_HF_Approx]
+  , _methods_qc_HF  :: [QC_HF_Shell]
   , _methods_qc_DFT :: [QC_DFT_Functional]
   , _methods_qc_MPN :: [QC_MPN_Order]
+  , _methods_qc_CC  :: [QC_CC_Order]
   , _methods_qc_CAS :: [QC_CAS_Approx]
   }
 makeLenses ''Methods
+
+-- | Generate a string for the possible different wavefunction types
+s :: [Shell] -> String
+s shell = "(" ++ concatMap shellString shell ++ ") "
+  where
+    shellString :: Shell -> String
+    shellString a
+      | Restricted == a = "r"
+      | Unrestricted == a = "u"
+      | RestrictedOpen == a = "o"
+      | otherwise = ""
+
+hfShell :: Shell -> String
+hfShell shell
+  | shell == Restricted = "RHF"
+  | shell == Unrestricted = "UHF"
+  | shell == RestrictedOpen = "ROHF"
+
 instance Show Methods where
   show a =
     "Methods \n" ++
     "  ├── Semiempiricism \n" ++ concatMap (\x ->
-    "  │     ├── Hamiltonian: " ++ show (x ^. qc_se_Method) ++ "\n") (a ^. methods_qc_SE) ++
+    "  │     ├── Hamiltonian: " ++ (s $ x ^. qc_se_Shell) ++ show (x ^. qc_se_Method) ++ "\n") (a ^. methods_qc_SE) ++
     "  │ \n" ++
+    {-
     "  ├── Hartree-Fock \n" ++ concatMap (\x ->
-    "  │     ├── Approximation: " ++ show (x ^. qc_hf_Approx) ++ "\n") (a ^. methods_qc_HF) ++
+    "  │     ├── Approximation: " ++ (s $ x ^. qc_hf_Shell) ++ show (x ^. qc_hf_Approx) ++ "\n") (a ^. methods_qc_HF) ++
     "  │ \n" ++
-    "  ├── DFT \n" ++ concatMap (\x ->
-    "  │     ├── Functional: " ++ show (x ^. qc_dft_Functional) ++ "\n" ++ concatMap (\y ->
+    -}
+    "  ├── Hartree-Fock\n" ++ concatMap (\x ->
+    "  │     ├── Shell: " ++ (hfShell $ x ^. qc_hf_Shell) ++ "\n" ++ concatMap (\y ->
+    "  │     │     ├── Approx: " ++ show y ++ "\n") (x ^. qc_hf_Approx)) (a ^. methods_qc_HF) ++
+    "  │ \n" ++
+    "  ├── Density Functional Theory \n" ++ concatMap (\x ->
+    "  │     ├── Functional: " ++ (s $ x ^. qc_dft_Shell)++ show (x ^. qc_dft_Functional) ++ "\n" ++ concatMap (\y ->
     "  │     │     ├── Approximation: " ++ show y ++ "\n") (x ^. qc_dft_Approx)) (a ^. methods_qc_DFT) ++
     "  │ \n" ++
     "  ├── Moller-Plesset \n" ++ concatMap (\x ->
     "  │     ├── Order: " ++ show (x ^. qc_mpn_Order) ++ "\n" ++ concatMap (\y ->
-    "  │     │     ├── Flavour: " ++ show (y ^. qc_mpn_Flavour) ++ "\n" ++ concatMap (\z ->
+    "  │     │     ├── Flavour: " ++ (s $ y ^. qc_mpn_Shell) ++ show (y ^. qc_mpn_Flavour) ++ "\n" ++ concatMap (\z ->
     "  │     │     │     ├── Approximation: " ++ show z ++ "\n") (y ^. qc_mpn_Approx)) (x ^. qc_mpn_Flavour')) (a ^. methods_qc_MPN) ++
     "  │ \n" ++
+
+    "  ├── Coupled Cluster \n" ++ concatMap (\x ->
+    "  │     ├── Order: " ++ show (x ^. qc_cc_Order) ++ "\n" ++ concatMap (\y ->
+    "  │     │     ├── Flavour: " ++ (s $ y ^. qc_cc_Shell) ++ (show $ y ^. qc_cc_Flavour) ++ "\n" ++ concatMap (\z ->
+    "  │     │     │     ├── Approximation: " ++ show z ++ "\n")
+    (y ^. qc_cc_Approx)
+    ) (x ^. qc_cc_Flavour')
+    ) (a ^. methods_qc_CC) ++
+    "  │\n" ++
     "  ├── CASSCF \n" ++ concatMap (\x ->
     "        ├── Approximation: " ++ show (x ^. qc_cas_Approx) ++ "\n") (a ^. methods_qc_CAS)
+
 
 --------------------------------------------------------------------------------
 -- test Types for priting. To be removed later
 --------------------------------------------------------------------------------
 
 testSE =
-  [ QC_SE_Method SE_PM6
-  , QC_SE_Method SE_XTB1
+  [ QC_SE_Method
+      { _qc_se_Method = SE_PM6
+      , _qc_se_Shell =
+          [ Restricted
+          , Unrestricted
+          ]
+      }
+  , QC_SE_Method
+      { _qc_se_Method = SE_XTB1
+      , _qc_se_Shell =
+          [ Restricted
+          , Unrestricted
+          ]
+      }
   ] :: [QC_SE_Method]
 
 testHF =
-  [ QC_HF_Approx HF_RIJK
-  , QC_HF_Approx HF_RIJCOSX
-  ] :: [QC_HF_Approx]
+  [ QC_HF_Shell
+      { _qc_hf_Shell = RestrictedOpen
+      , _qc_hf_Approx =
+          [ HF_None
+          , HF_RIJK
+          , HF_RIJCOSX
+          , HF_RIJONX
+          ]
+      }
+  ] :: [QC_HF_Shell]
 
 testDFT =
   [ QC_DFT_Functional
       { _qc_dft_Functional = DFT_TPSS
+      , _qc_dft_Shell = [Restricted, Unrestricted, RestrictedOpen]
       , _qc_dft_Approx =
           [ DFT_RIJ
           , DFT_RIJCOSX
@@ -437,6 +575,7 @@ testDFT =
       }
   , QC_DFT_Functional
       { _qc_dft_Functional = DFT_M06
+      , _qc_dft_Shell = [Restricted, Unrestricted, RestrictedOpen]
       , _qc_dft_Approx =
           [ DFT_RIJONX
           , DFT_RIJK
@@ -451,6 +590,7 @@ testMPN =
       , _qc_mpn_Flavour' =
           [ QC_MPN_Flavour
               { _qc_mpn_Flavour = MP_OO
+              , _qc_mpn_Shell = [Restricted, Unrestricted]
               , _qc_mpn_Approx =
                   [ MP_None
                   , MP_RI
@@ -459,6 +599,7 @@ testMPN =
               }
           , QC_MPN_Flavour
               { _qc_mpn_Flavour = MP_Conv
+              , _qc_mpn_Shell = [Restricted, Unrestricted]
               , _qc_mpn_Approx =
                   [ MP_RI
                   , MP_DLPNO
@@ -471,6 +612,7 @@ testMPN =
       , _qc_mpn_Flavour' =
           [ QC_MPN_Flavour
               { _qc_mpn_Flavour = MP_OO
+              , _qc_mpn_Shell = [Restricted, Unrestricted]
               , _qc_mpn_Approx =
                   [ MP_RI
                   , MP_DLPNO
@@ -478,8 +620,25 @@ testMPN =
               }
           , QC_MPN_Flavour
               { _qc_mpn_Flavour = MP_Conv
+              , _qc_mpn_Shell = [Restricted, Unrestricted]
               , _qc_mpn_Approx =
                   [ MP_None
+                  ]
+              }
+          ]
+      }
+  ]
+
+testCC =
+  [ QC_CC_Order
+      { _qc_cc_Order = CC_SD
+      , _qc_cc_Flavour' =
+          [ QC_CC_Flavour
+              { _qc_cc_Flavour = CC_Conv
+              , _qc_cc_Shell = [Restricted, Unrestricted]
+              , _qc_cc_Approx =
+                  [ CC_None
+                  , CC_RI
                   ]
               }
           ]
@@ -496,5 +655,6 @@ testMethods = Methods
   , _methods_qc_HF = testHF
   , _methods_qc_DFT = testDFT
   , _methods_qc_MPN = testMPN
+  , _methods_qc_CC = testCC
   , _methods_qc_CAS = testCAS
   }
