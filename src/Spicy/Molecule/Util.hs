aaff39eff83e6e04944888bb64ca93aa61be37cb
Phillip Seeber <seeber@rlyeh.ipc3.uni-jena.de>
2019-08-23 17:46:57 +0200

    reindexing of whole molecule by intmap

--- src/Spicy/Molecule/Util.hs
+++ src/Spicy/Molecule/Util.hs
@@ -12,38 +12,163 @@ This module provides functions to manipulate basic data structures of 'Molecule'
 module Spicy.Molecule.Util
 (
 ) where
-import Spicy.Types
-import qualified Data.IntMap as IM
-import Data.IntMap (IntMap)
-import qualified Data.Vector as VB
-import Lens.Micro.Platform
+import           Data.IntMap         (IntMap, (!?))
+import qualified Data.IntMap         as IM
+import           Data.IntSet         (IntSet, (\\))
+import qualified Data.IntSet         as IS
+import           Data.Maybe
+import qualified Data.Vector         as VB
+import           Lens.Micro.Platform
+import           Spicy.Types
+
+{-
+"IS" = IntSet
+"IM" = IntMap
+"oK" = old key
+"nK" = new key
+"RI" = replaced index
+"oS" = old set
+"nS" = new set
+"oA" = old atom
+"nA" = new atom
+-}
 
 {-|
-Calculate new indices for 'Molecule's in the 'Individual' scheme, where each fragment is counted by
-itself without awareness for other molecules. This relies on sane bonds in the 'Molecule'. If
-somewhere bonds are defined to non existing atoms, these bonds will be lost.
+Reindex a complete 'Molecule', including all its deeper layers in '_molecule_SubMol') by mappings
+from a global replacement Map, mapping old to new indices. This function assumes, that your molecule
+is sane in the overall assumptions of this program. This means that the lower layers obey the
+counting scheme of the atoms of the higher layers and pseudoatoms come last.
 -}
--- "RI" = Replaced Indices
-reIndexLocal :: Molecule -> Molecule
-reIndexLocal mol =
-  let molRI        = reIndFrag mol
-      molRISubMols = molRI ^. molecule_SubMol
-  in  if molRISubMols == VB.empty
-        then molRI & molecule_SubMol .~ (VB.map reIndexLocal molRISubMols)
-        else molRI
-  where
-    -- Reindex a layer of 'Molecule' but ignore other layers.
-    reIndFrag :: Molecule -> Molecule
-    reIndFrag m =
-      let atoms      = m ^. molecule_Atoms
-          nAtoms     = VB.length nAtoms
-          indicesOld = VB.map _atom_Index atoms
-          indicesNew = VB.generate nAtoms (\i -> i)
-          indReplMap = IM.fromList . VB.toList $ VB.zip indicesOld indicesNew
-          bonds      = m ^. molecule_Bonds
-          atomsRI    = VB.zipWith (\a i -> a & atom_Index .~ i) atoms indicesNew
-          --bondsRI    =
+reIndexMolecule :: IntMap Int -> Molecule -> Either String Molecule
+reIndexMolecule repMap mol = do
+  -- Reindex the current layer only
+  molRI <- reIndexMoleculeLayer repMap mol
+  -- Get the submolecules
+  let subMols = mol ^. molecule_SubMol
+    -- If the molecule has no submolecules:
+  if VB.null subMols
+    -- Then we are done.
+    then Right molRI
+    -- Else we need to reindex the deeper layers also.
+    else do
+      subMolsRI <- VB.sequence . VB.map (reIndexMolecule repMap) $ subMols
+      reIndexMolecule repMap $ molRI & molecule_SubMol .~ subMolsRI
 
-      in  m
-            & molecule_Atoms   .~ atomsRI
-            & molecule_IndType .~ Individual
+
+
+{-|
+Reindex the '_molecule_Atoms' and '_molecule_Bonds' of a single layer of a molecule (ignoring
+anything in the '_molecule_SubMol' field). While the completeness of the reindexing is checked and
+incompleteness of the replacement 'IntMap' 'Int' will result in 'Left' 'String', it is not checked
+if the 'Atom''s indexing is sane and indices are unique.
+-}
+reIndexMoleculeLayer ::
+     IntMap Int             -- ^ 'IntMap' with mappings from old indices to new indices (bonds and
+                            --   atoms).
+  -> Molecule               -- ^ Molecule to reindex.
+  -> Either String Molecule -- ^ 'Left' 'String' in case the mapping from old keys to new keys is
+                            --   incomplete.
+reIndexMoleculeLayer repMap mol
+  -- If old Atom indices cannot be completely replaced by the Map:
+  | not $ checkRepMapCompleteIS repMap (IM.keysSet $ mol ^. molecule_Atoms) =
+      Left "reIndexMoleculeLayer: The remapping of indices is not complete for the atom indices."
+  -- If old bond type data cannot be completely replaced by the Map:
+  | not $ checkRepMapCompleteIMIS repMap (mol ^. molecule_Bonds)            =
+      Left "reIndexMoleculeLayer: The remapping of indices is not complete for the bond data."
+  -- If both checks went fine:
+  | otherwise                                                               =
+      Right $ mol
+        -- Use the (%~) lens to update the atoms indices of a molecule with new indices.
+        & molecule_Atoms %~ replaceIMKeys repMap
+        -- Use the (%~) lens to update keys and values (IntSet) of the bond type data structure.
+        & molecule_Bonds %~ replaceIMIS repMap
+
+{-|
+Check if 'IntMap' is complete to replace all values in an 'IntSet' (any old 'IS.Key' can be replaced
+by a new 'IS.Key').  Gives 'True' if complete, 'False' if the replacement 'IntMap' 'Int' has holes.
+-}
+checkRepMapCompleteIS ::
+     IntMap Int -- ^ 'IntMap' containing the mapping from old 'IS.Key's to new 'IS.Key's.
+  -> IntSet     -- ^ 'IntSet' in which values should be replaced.
+  -> Bool       -- ^ Result.
+checkRepMapCompleteIS repMap is =
+  let -- Keys, that can be replaced
+      repKeys  = IM.keysSet repMap
+      -- Keys that shall be replaced minus keys that can be replaced
+      lostKeys = is \\ repKeys
+  in  IS.null lostKeys
+
+{-|
+Check if 'IntMap' is complete to replace all 'IM.Key's from the old 'IntMap' by new 'IM.Key's. Gives
+'True' if the 'IntMap' with replacements is complete and 'False' otherwise.
+-}
+checkRepMapCompleteIM ::
+     IntMap Int -- ^ 'IntMap' containing the mapping from old 'IS.Key's to new 'IS.Key's.
+  -> IntMap a   -- ^ 'IntMap' in which 'IM.Key's should be replaced.
+  -> Bool       -- ^ Result.
+checkRepMapCompleteIM repMap im =
+  let -- Keys, that can be replaced
+      repKeys = IM.keysSet repMap
+      -- Keys that shall be replaced
+      oldKeys = IM.keysSet im
+      -- Keys that cannot be replaced
+      lostKeys = oldKeys \\ repKeys
+  in  IS.null lostKeys
+
+
+{-|
+Check if 'IntMap' is complete to replace all values in an 'IntMap' 'InSet' type construction
+(replacing both the lookup keys in the 'IntMap', as well as all values in the 'IntSet'). Gives
+'True' if complete and 'False' otherwise.
+-}
+checkRepMapCompleteIMIS ::
+     IntMap Int    -- ^ 'IntMap' containing the mapping from old 'IS.Key's to new 'IS.Key's.
+  -> IntMap IntSet -- ^ 'IntMap' 'IntSet' in which values and keys should be replaced.
+  -> Bool          -- ^ Result.
+checkRepMapCompleteIMIS repMap imis =
+  let -- All values, that appear in the union of all IntSet
+      oldSets    = IS.unions imis
+  in  checkRepMapCompleteIM repMap imis && checkRepMapCompleteIS repMap oldSets
+
+{-|
+Replace all 'IS.Key's from an 'IntSet' according to mappings from an 'IntMap' 'Int'. Entries, that
+cannot be found in the 'IntMap' will no be changed.
+-}
+replaceIS ::
+     IntMap Int -- ^ 'IntMap' containing the mapping from old 'IS.Key's to new 'IS.Key's.
+  -> IntSet     -- ^ 'IntSet' to be modified.
+  -> IntSet     -- ^ Resulting new 'IntSet'.
+replaceIS repMap is =
+  IS.map (\oK ->
+    let nK = repMap !? oK
+    in  fromMaybe oK nK
+  ) is
+
+{-|
+Replace all 'IM.Key's in an 'IntMap' according to mappings from an 'IntMap' 'Int'. Entries that
+cannot be found in the 'IntMap' will not be changed.
+-}
+replaceIMKeys ::
+     IntMap Int -- ^ 'IntMap' containing the mapping from old 'IM.Key's to new 'IM.Key's.
+  -> IntMap a   -- ^ 'IntMap' in which 'IM.Key's shall be replaced.
+  -> IntMap a   -- ^ Resulting new 'IntMap' with replaced 'IM.Key's.
+replaceIMKeys repMap im =
+  IM.mapKeys (\oK ->
+    let nK = repMap !? oK
+    in  fromMaybe oK nK
+  ) im
+
+{-|
+Replace all lookup-'IM.Key's of an 'IntMap' 'IntSet' and all values in the 'IntSet' by a given
+mapping from an 'IntMap'. Entries that cannot be found in the 'IntMap' will not be changed.
+-}
+replaceIMIS ::
+     IntMap Int    -- ^ 'IntMap' containing the mapping from old 'IS.Key's to new 'IS.Key's.
+  -> IntMap IntSet -- ^ Original structure, which to replace both keys and values.
+  -> IntMap IntSet -- ^ modified structure.
+replaceIMIS repMap imis = undefined
+  -- Replace all values in all IntSet
+    IM.map (replaceIS repMap)
+  -- Replace all lookup keys
+  . replaceIMKeys repMap
+  $ imis
