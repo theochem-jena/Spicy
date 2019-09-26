b2b32f7ddc07c60926ebadb6b6d27fc47a87195b
Phillip Seeber <seeber@rlyeh.ipc3.uni-jena.de>
2019-09-26 15:42:57 +0200

    Adds empty module for input file writer for Psi4

---
+++ src/Spicy/Wrapper/Psi4/Internal/InputWriter.hs
@@ -0,0 +1,17 @@
+{-|
+Module      : Spicy.Wrapper.Psi4.Internal.InputWriter
+Description : Generator for Psi4 input files
+Copyright   : Phillip Seeber, 2019
+License     : GPL-3
+Maintainer  : phillip.seeber@uni-jena.de
+Stability   : experimental
+Portability : POSIX, Windows
+
+This module provides a translator from 'Wrapper' to an actual Psi4 input file. The translation is
+carefully checked to be sensible and actually doable by Psi4.
+-}
+
+module Spicy.Wrapper.Psi4.Internal.InputWriter
+() where
+import Spicy.Wrapper.Internal.Types
+import Control.Lens
